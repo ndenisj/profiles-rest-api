4d725c4213114b18d9acc712c7bb3719ecd4a847
Denis John <denis.john.ndarake@gmail.com>
2019-09-26 05:48:37 +0100

    Added User feed API

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -6,6 +6,7 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewset, base_name='hello-viewset')
 router.register('profile', views.UserProfileViewSet)
+router.register('feed', views.UserProfileFeedViewSet)
 
 urlpatterns = [
     path('hello-api/', views.HelloApiView.as_view()),
